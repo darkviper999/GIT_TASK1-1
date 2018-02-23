6487ff67c9a41ffe6ff940c189647f829792c414
JoeKooler <jojesin@gmail.com>
2018-02-23 18:13:12 +0700

    โพลิวัวฟิซึ่ม

--- Polymorphism/incomplete_code.py
+++ Polymorphism/incomplete_code.py
@@ -27,7 +27,7 @@ class Walk( Transportation ):
 
 travel_cost = 0
 
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
+trip = [ Walk("KMITL","KMITL SCB Bank",0.6)]
          #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
          #Train("Ladkrabang Station","Payathai Station",40,6),
          #Taxi("Payathai Station","The British Council",3) ]
